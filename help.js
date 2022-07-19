39bfce2376d9fd242e49c56be457543a8b51d806
Siva <prabala.siva12@gmail.com>
2022-07-19 21:06:37 +0530

    Push

---
+++ help.js
@@ -0,0 +1,67 @@
+/*
+COVID-19 Reporter
+
+Help.js - script for Help file - COVID-19 Reporter
+Author: Siva
+Email: Siva@yahoo.com
+COVID-19 Reporter Copyright (c) 2020 Siva. All Rights Reserved
+*/
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    
+    document.getElementById('btnSummary').addEventListener('click', function (){
+        let summaryTopics = document.getElementById('summaryTopics');
+    
+        if(summaryTopics.style.display == ""){
+            summaryTopics.style.display = "block";
+        }
+        else{
+            summaryTopics.style.display = "";
+        }
+    });
+
+    document.getElementById('btnDetailed').addEventListener('click', function (){
+        let div = document.getElementById('detailed');
+    
+        if(div.style.display == ""){
+            div.style.display = "block";
+        }
+        else{
+            div.style.display = "";
+        }
+    });
+    document.getElementById('btnTimeSeries').addEventListener('click', function (){
+        let div = document.getElementById('timeSeries');
+
+        if(div.style.display == ""){
+            div.style.display = "block";
+        }
+        else{
+            div.style.display = "";
+        }
+    });
+    document.getElementById('btnAllCountries').addEventListener('click', function (){
+        let div = document.getElementById('allCountries');
+
+        if(div.style.display == ""){
+            div.style.display = "block";
+        }
+        else{
+            div.style.display = "";
+        }
+    });
+    document.getElementById('btnOptions').addEventListener('click', function (){
+        let div = document.getElementById('options');
+    
+        if(div.style.display == ""){
+            div.style.display = "block";
+        }
+        else{
+            div.style.display = "";
+        }
+    });
+
+
+});
+
